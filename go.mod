ca56521bc88a4e2fc061aab0b55436d3ea25a4a5
Masataka Hisasue <masataka.hisasue@gmail.com>
2024-08-22 14:17:10 +0900

    Feat use chi

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/HMasataka/golang-tls
 
 go 1.21.12
+
+require github.com/go-chi/chi/v5 v5.1.0
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello, world")
-	})
+	r := chi.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r.Use(middleware.Logger)
+
+	r.Get("/", helloWorld)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		panic(err)
+	}
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	if _, err := w.Write([]byte("hello, world")); err != nil {
+		log.Print(err)
+	}
 }
